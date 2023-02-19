8f977db7c6fa3547b8744479581d7ae0a26ef9bc
APPAtacker.py <77343301+K-GOKULAPPADURAI@users.noreply.github.com>
2023-02-19 21:32:35 +0530

    Update MainOfGokul.py

--- MainOfGokul.py
+++ MainOfGokul.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 import time
-net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+net = cv2.dnn.readNet("weights/yolov3.weights", "yolov3.cfg")
 classes = ["Weapon"]
 with open("obj.names", "r") as f:
     classes = [line.strip() for line in f.readlines()]
